833b94c6b43df701b72a296328094d9972cf538b
Vitor Paisante <vitor.paisante@sambatech.com.br>
2018-07-03 20:13:16 -0300

    Added setup base

---
+++ python/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="samba-chassis",
+    version="1.0.0",
+    author="Vitor Mendes Paisante",
+    author_email="vitor.paisante@sambatech.com.br",
+    description="Framework for building microservices at SambaTech",
+    license="Copyright SambaTech",
+    keywords="microsservices framework library",
+    url="http://www.sambatech.com.br",
+    packages=['samba-chassis'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.7"
+    ],
+    install_requires=[],
+    python_requires=">=2.7"
+)
